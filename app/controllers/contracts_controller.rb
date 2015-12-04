a64f2aece137d630d3c880a220a510d9bb1c9cbc
yokogi <yokogi@e-reverse.com>
2015-12-04 17:09:21 +0900

    search end and style

--- app/controllers/contracts_controller.rb
+++ app/controllers/contracts_controller.rb
@@ -20,14 +20,53 @@ class ContractsController < ApplicationController
   def index
 
 # branch prep_search 2015/12/02
-    if params[:search]
-      jyoken = params[:search]
-      sql = 'SELECT C.* FROM contracts C WHERE itakumoto LIKE ?', "%#{jyoken}%"
-     #byebug
-     #@contracts = Contract.search(params[:search])
-     @contracts = Contract.paginate_by_sql(sql,:page => params[:page], :per_page => 5)
 
+    sql = 'SELECT C.* FROM contracts C'
+
+
+    conditionflg = 0
+
+    if params[:search].present? && params[:searchsaki].present?
+#      jyoken = params[:search]
+#      sql = sql + ' WHERE itakumoto LIKE ?', "%#{jyoken}%"
+#      conditionflg = 1
+
+#     if params[:searchsaki]
+      jyoken = params[:search]
+      jyoken2 = params[:searchsaki]
+#      sql = sql + ' WHERE itakumoto LIKE ?', "%#{jyoken}%"
+#      sql = sql + ' AND itakusaki LIKE ?', "%#{jyoken2}%"
+#      sql = sql + ' WHERE itakumoto LIKE ?', "%#{jyoken}%", ' AND itakusaki LIKE ?', "%#{jyoken2}%"
+      sql = sql + ' WHERE itakumoto LIKE ? AND itakusaki LIKE ?', "%#{jyoken}%", "%#{jyoken2}%"
+#      sql = sql + ' WHERE itakumoto LIKE "%1%" AND itakusaki LIKE "%2%"'
+#     else
+#      # do nothing
+#     end
+      #byebug
+      #@contracts = Contract.search(params[:search])
+      #@contracts = Contract.paginate_by_sql(sql,:page => params[:page], :per_page => 5)
+      conditionflg = 1
 # branch prep_search 2015/12/02 end
+    else
+
+     if params[:searchsaki].present?
+#      jyoken = params[:search]
+      jyoken2 = params[:searchsaki]
+      sql = sql + ' WHERE itakusaki LIKE ?', "%#{jyoken2}%"
+      conditionflg = 1
+     elsif params[:search].present?
+      jyoken = params[:search]
+      #jyoken2 = params[:searchsaki]
+      sql = sql + ' WHERE itakumoto LIKE ?', "%#{jyoken}%"
+      conditionflg = 1
+     else
+
+     end
+
+    end
+
+    if conditionflg == 1
+     @contracts = Contract.paginate_by_sql(sql,:page => params[:page], :per_page => 5)
     else
      @contracts = Contract.paginate(page: params[:page],:per_page => 5)
     end
